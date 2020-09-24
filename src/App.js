cda3914353ca910672b516f91a889364aebe19c4
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2020-09-24 13:22:08 -0700

    added about component with placeholders for imgs/bio that will come later

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Logo from './components/Logo';
+import About from './components/About'
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './App.css';
@@ -21,6 +22,7 @@ function App() {
       <div>
         <Logo />
         <h1>Welcome to Dr. Ques' Art Gallery and Store</h1>
+        <About />
       </div>
 
       <Footer />
