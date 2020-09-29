a7019a9b15b45f6c91c3f6ea7f0aad1f397164a5
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2020-09-29 12:17:50 -0700

    udpated styling to footer, didnt work, so reverted and added a margin top for some spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {Nav, Navbar} from 'react-bootstrap'
+import {Nav, Navbar, Container, Row, Col} from 'react-bootstrap'
 
 const Footer = () => {
     return (
         <div>
-            <Navbar sticky='bottom'  className='justify-content-end footer'>
+            <Navbar sticky='bottom'  className='justify-content-end footer mt-4'>
                 <Nav>
                     <Nav.Link className='copywrite' href='https://zackbarovsky.dev/' target='_blank'>© Zack Barovsky 2020</Nav.Link>
                 </Nav>
-                <Nav className='socialMediaLinks'>
+                <Nav className='justify-content-end socialMediaLinks'>
                     <Nav.Link href='https://www.instagram.com/docterques/' target='_blank'>Instagram</Nav.Link>
                     <Nav.Link href='https://www.twitch.tv/docterques' target='_blank' >Twitch</Nav.Link>
                 </Nav>
